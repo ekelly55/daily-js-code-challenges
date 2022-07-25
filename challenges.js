20b2b04ca559749909c7c878ca4e77b99d4de2c6
ekelly55 <eamonn.michael.kellyr@email.com>
2022-07-25 15:42:15 -0400

    solve addOne function

--- challenges.js
+++ challenges.js
@@ -33,7 +33,9 @@ addOne(1) //=> 2
 addOne(-5) //=> -4
 -----------------------------------------------------------------*/
 // Your solution for 01-addOne here:
-
+function addOne(num) {
+  return num +1;
+}
 
 
 
